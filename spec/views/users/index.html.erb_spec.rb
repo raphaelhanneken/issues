dbcbe90a7f77402ca1ba852b9908a327027d421f
Raphael Hanneken <raphaelhanneken@me.com>
2016-04-06 19:13:02 +0200

    Update the users spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "users/index", type: :view do
   let(:user_01) { FactoryGirl.build_stubbed(:user, firstname: 'Robert', lastname: 'Plant') }
   let(:user_02) { FactoryGirl.build_stubbed(:user, firstname: 'Jimmy', lastname: 'Page') }
-      
+
   before(:each) do
     allow(view).to receive(:current_user).and_return(FactoryGirl.build_stubbed(:user))
     assign(:users, [user_01, user_02])
@@ -15,20 +15,20 @@ RSpec.describe "users/index", type: :view do
   end
 
   it 'displays a link to each user' do
-    expect(response).to have_link('Plant, Robert', href: user_path(user_01))
-    expect(response).to have_link('Page, Jimmy', href: user_path(user_02))
+    expect(response).to have_link('Robert Plant', href: user_path(user_01))
+    expect(response).to have_link('Jimmy Page', href: user_path(user_02))
   end
-  
+
   it 'does not display delete links' do
     expect(response).not_to have_link('Delete')
   end
-  
+
   describe 'for users with admin privileges' do
     before(:each) do
       allow(view).to receive(:current_user).and_return(FactoryGirl.build_stubbed(:user, admin: true))
       render
     end
-    
+
     it 'displays a delete link for each user' do
       expect(response).to have_link('Delete', href: user_path(user_01))
       expect(response).to have_link('Delete', href: user_path(user_02))
