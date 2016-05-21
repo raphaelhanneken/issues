3c98460f51ed3d950831b5310dab5e52408faf07
Raphael Hanneken <raphaelhanneken@me.com>
2016-05-21 16:33:03 +0200

    Devise::ParameterSanitizer: Replace depricated #for with #permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u|
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:email, :firstname, :lastname, :password, :password_confirmation)
       end
     end
