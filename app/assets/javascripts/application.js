39da6233b9f0cc17378c3a4fb095a91cec06df4a
Raphael Hanneken <raphaelhanneken@me.com>
2016-06-10 13:25:13 +0200

    NProgress typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,4 +36,4 @@
 
 $(document).on('page:fetch',   function() { NProgress.start(); });
 $(document).on('page:change',  function() { NProgress.done(); });
-$(document).on('page:restore', function() { NSProgress.remove(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
